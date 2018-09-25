259947d9550d1534812b23f36bca1bcb57a22771
Marcin Wójcik <mrc.wojcik@gmail.com>
2018-09-25 22:23:28 +0200

    Create Project - first java app

---
+++ Tutorial1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mrc
+ *
+ */
+module tutorial1 {
+}
---
+++ Tutorial1/src/tut1/application.java
@@ -0,0 +1,10 @@
+package tut1;
+
+public class application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! ");
+	}
+
+}
