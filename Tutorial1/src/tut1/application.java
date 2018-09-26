9846a37af6b95667f0f22cad1fa2f82625304e45
Marcin Wójcik <mrc.wojcik@gmail.com>
2018-09-26 19:22:43 +0200

    variables, for

--- Tutorial1/src/tut1/application.java
+++ Tutorial1/src/tut1/application.java
@@ -1,10 +1,14 @@
 package tut1;
 
 public class application {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World! ");
+		/* int myAge = 28;
+		String name = "Marcin";
+		System.out.println("Mam na imiê " + name + ". Mam " + myAge + " lat"); */
+		
+		for(int i=0; i < 5; i++) {
+			System.out.println(i);
+		}
 	}
 
 }
